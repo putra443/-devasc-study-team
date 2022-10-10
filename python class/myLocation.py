1a17a79f4bca5b0b9772b0cddd40f2535a96770a
putra443 <putra443@gmail.com>
2022-10-10 07:42:19 +0000

    adding folder python class to repo

---
+++ python class/myLocation.py
@@ -0,0 +1,14 @@
+class Location : 
+    def __init__(self, name, country):
+        self.name = name
+        self.country = country
+    def myLocation(self):
+        print("Hi, my name is "+ self.name +" and I live in " + self.country + ".")
+
+loc1 = Location("Tomas", "Portugal")
+loc2 = Location("Ying", "China")
+loc3 = Location ("Amare", "Kenya")
+
+loc1.myLocation()
+loc2.myLocation()
+loc3.myLocation()
